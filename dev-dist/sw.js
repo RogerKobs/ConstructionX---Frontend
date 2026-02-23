f02054701a0a27b0e4ea8ef5321046e61b0d35b9
Filip Radotović <76910109+fradotovic@users.noreply.github.com>
2026-02-23 21:28:59 +0100

    WIP

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rrjkel9nfm8"
+    "revision": "0.4hkeupa5dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
